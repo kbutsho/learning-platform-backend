a9d3392157c865fe3b6cde1af6747da6e97ca5b1
Kaushik Biswas <kbutsho@gmail.com>
2024-05-17 17:39:51 +0600

    create user topic and category model migration and controller

--- database/migrations/2024_05_17_113025_create_topics_table.php
+++ database/migrations/2024_05_17_113025_create_topics_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('topics', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
+            $table->name('string');
+            $table->category_id('string');
+            $table->heading('string');
+            $table->description('text')->nullable();
+            $table->name('string');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('topics');
     }
 };
